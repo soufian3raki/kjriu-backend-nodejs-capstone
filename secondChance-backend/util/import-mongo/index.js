f70723459574cd5e5755ee3685134a9e5c051bea
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-03-15 17:13:49 +1100

    Update index.js

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -8,7 +8,7 @@ let filename = `${__dirname}/secondChanceItems.json`;
 const dbName = 'secondChance';
 const collectionName = 'secondChanceItems';
 
-// notice you have to load the array of gifts into the data object
+// notice you have to load the array of items into the data object
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs;
 
 // connect to database and insert data into the collection
