1e298c97da5583170d39feb860ac6d135d24be8a
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-03-22 08:43:41 +1100

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName = "giftdb";
+const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
     if (dbInstance){
